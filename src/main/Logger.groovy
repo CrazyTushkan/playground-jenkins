3991b58c48a1259ba7b53473a5e0689b4a2b9e99
CrazyTushkan <crazytushkan@gmail.com>
2019-01-18 17:45:15 +0300

    Move logger to library

--- src/main/Logger.groovy
+++ src/main/Logger.groovy
@@ -1,38 +1,54 @@
 package main
 
 import org.jenkinsci.plugins.workflow.cps.CpsThreadGroup
+import hudson.logging.LogRecorder
+import jenkins.model.Jenkins
+import java.util.logging.Logger as JavaLogger
+import java.util.logging.Level
+
 
 class Logger {
     def thread
-    def script
 
     Logger() {
         this.thread = CpsThreadGroup.current()
+        createRecorder('TestRecorder')
+        createTarget('TestRecorder', 'TestTarget', Level.ALL)
+        JavaLogger.getLogger('TestTarget').info('Test message')
     }
 
-    // @NonCPS
-    // private LogRecorder getRecorder(String recorder) {
-    //     Map<String, LogRecorder> logRecorders = Jenkins.get().getLog().logRecorders
-    //     return logRecorders.get(recorder)
-    
-    // @NonCPS
-    // private void createRecorder(String name) {
-    //     LogRecorder recorder = get
-    //     if (!recorder) {
-    //         recorder = new LogRecorder(name)
-    //         logRecorders.put(name, recorder)
-    //     }
-    // }
-
-    // @NonCPS
-    // private void createTarget(String recorderName) {
-    //     Map<String, LogRecorder> logRecorders = Jenkins.get().getLog().logRecorders
-    //     LogRecorder recorder = logRecorders.get(name)
-        
-    // }
+    @NonCPS
+    private Map<String, LogRecorder> getRecorders() {
+        return Jenkins.get().getLog().logRecorders
+    }
     
     @NonCPS
-    public void log(logLevel, message) {
+    private void createRecorder(String name) {
+        Map<String, LogRecorder> recorders = getRecorders()
+        if(!recorders.get(name)) {
+            LogRecorder recorder = new LogRecorder(name)
+            recorders.put(name, recorder)
+            log('LogRecorder created')
+        }
+    }
+
+    @NonCPS
+    private void createTarget(String recorderName, String targetName, Level targetLevel) {
+        LogRecorder recorder = getRecorders().get(recorderName)
+        LogRecorder.Target newTarget = new LogRecorder.Target(targetName, targetLevel)
+        List<LogRecorder.Target> targets = new ArrayList<LogRecorder.Target>(recorder.targets.getView())
+        for(target in recorder.targets.getView()) {
+            if(target.getName() == targetName) {
+                targets.remove(target)
+            }
+        }
+        targets.add(newTarget)
+        recorder.targets.replaceBy(targets)
+        log('LogRecorder targets updated')
+    }
+    
+    @NonCPS
+    public void log(message) {
         def stream = this.thread.execution.owner.listener.getLogger()
         stream.println(message)
     }
